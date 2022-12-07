edc632ea3057acefb27e319fccae4a6e663b7afa
rmtkable <rmtkable@gmail.com>
2022-12-07 09:44:40 -0500

    pushing connection to backend repo

--- src/config.js
+++ src/config.js
@@ -2,4 +2,6 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const BACKEND_URL=process.env.NODE_ENV === 'development'?"http://localhost:5000/": "https://capstone-copy.herokuapp.com/"
+// export const BACKEND_URL=process.env.NODE_ENV === 'development'?"http://localhost:5000/": "https://capstone-copy.herokuapp.com/"
+
+export const BACKEND_URL = 'https://capstone-practice-backend.onrender.com/'
